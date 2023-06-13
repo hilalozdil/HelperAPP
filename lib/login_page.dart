7dcd3af1d7500fba7a56fa7c8fa6234efe835f86
Hilal Özdil <106959747+hilalozdil@users.noreply.github.com>
2023-06-13 15:46:46 +0300

    login page sayfası devamı

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,26 +6,60 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String? username;
+  String? password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(166, 148, 111, 1.0),
       resizeToAvoidBottomInset: false,
       body: Form(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: TextFormField(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
                 decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.brown),
-                  ),
-                ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                    labelText: "E-Posta ",
+                    labelStyle: TextStyle(color: Colors.brown),
+                    border: OutlineInputBorder()),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Eposta Giriniz";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  username == value;
+                },
               ),
-            )
-          ],
+              SizedBox(height: 10.0),
+              TextFormField(
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                    labelText: "Şifre ",
+                    labelStyle: TextStyle(color: Colors.brown),
+                    border: OutlineInputBorder()),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Şifrenizi Giriniz";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  username == value;
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
